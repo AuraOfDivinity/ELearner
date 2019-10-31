ffb10b82299b512475e49dda62bef5b24a948fd3
pyrecyter <sachiranuwanga123@gmail.com>
2019-10-31 13:53:38 +0530

    user auth

--- Backend/routes/api/user.js
+++ Backend/routes/api/user.js
@@ -1,11 +1,47 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('firebase/auth');
-const firestore = require('firebase/firestore');
 
-router.get('/',(req,res)=>{
-    firestore.collections
+var admin = require("firebase-admin");
+
+var serviceAccount = require("C:/Users/user/Downloads/service-account-file.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://elearner-2a954.firebaseio.com"
 });
 
+router.get('/', (req, res) => {
+
+    const users = [];
+
+    admin.auth().listUsers().then((user) => {
+        user.users.forEach((user) => {
+            users.push(user.toJSON())
+        });
+        res.send(users);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    admin.auth().getUser(uid = req.params.id).then((userData) => {
+        res.send(userData.toJSON());
+    }).catch((err) => {
+        res.status(404).send(err);
+    })
+});
+
+router.post('/', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    if (email == null|| password == null) res.status(400).send('Bad request body');
+    admin.auth().createUser({
+        email, password
+    }).then((user) => { res.send(user.toJSON()) }).catch((e) => {
+        res.status(400);
+    })
+});
+
+
+module.exports = router;
 
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,34 +1,17 @@
 const express = require('express');
 const app = express();
-const connectDB = require('./firebase/firebase');
+const bodyParser = require('body-parser')
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-
-//connecting to db
-connectDB();
-
-//Middleware 
-app.use(express.json({extended:false}));
-
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/',(req,res) => res.send('API Running'))
 
-
-
 //define Route 
-app.use('/api/students',require('./routes/api/students')); 
-app.use('/api/auth',require('./routes/api/auth')); 
-app.use('/api/adminauth',require('./routes/api/adminauth')); 
-app.use('/api/inauth',require('./routes/api/instructorauth'));
-app.use('/api/profile',require('./routes/api/profile')); 
-app.use('/api/instructorprofile',require('./routes/api/instructorprofile')); 
-app.use('/api/adminprofile',require('./routes/api/adminprofile')); 
-app.use('/api/instructor',require('./routes/api/instructor')); 
-app.use('/api/admin',require('./routes/api/admin')); 
-app.use('/api/post',require('./routes/api/post'));
-app.use('/api/course',require('./routes/api/course'));
-app.use('/api/assignment',require('./routes/api/assignment'))
-app.use('/api/lecture',require('./routes/api/lectureRoute'))
+app.use('/api/user',require('./routes/api/user'))
 
 
 //if port is taken changes to 5000
